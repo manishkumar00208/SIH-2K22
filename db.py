d18772c135baad64727b54253ff78ab2e1920a94
MANISH KUMAR CHINTHA <chintamanishkumar@gmail.com>
2022-08-14 16:06:02 +0530

    s

--- db.py
+++ db.py
@@ -1,1 +1,10 @@
-import
+import mysql.connector as c
+
+mydb = c.connect(host="LAPTOP-631I4VEI", user="manish", passwd="1234", database="aadhardetails")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("select * from students")
+
+for i in mycursor:
+    print(i)
